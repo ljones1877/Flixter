ffafd253260ac6e55adc96c55ab09ffbd201a6ec
ljones1877 <jlorin7@gmail.com>
2019-12-16 20:53:24 +0000

    Now new error

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,32 +1,11 @@
 CarrierWave.configure do |config|
-  config.storage    = :aws
-  config.aws_bucket = ENV["AWS_BUCKET"]
-  config.aws_acl    = "public-read"
-
-  # Optionally define an asset host for configurations that are fronted by a
-  # content host, such as CloudFront.
-  config.asset_host = 'http://example.com'
-
-  # The maximum period for authenticated_urls is only 7 days.
-  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
-
-  # Set custom options such as cache control to leverage browser caching.
-  # You can use either a static Hash or a Proc.
-  config.aws_attributes = -> { {
-    expires: 1.week.from_now.httpdate,
-    cache_control: 'max-age=604800'
-  } }
-
-  config.aws_credentials = {
-      access_key_id:     ENV["AWS_ACCESS_KEY"],
-      secret_access_key: ENV["AWS_SECRET_KEY"],
-      region:            ENV["AWS_REGION"]
+  config.fog_provider = 'fog/aws'                        
+  config.fog_public = false
+  config.fog_credentials = {
+    provider:              'AWS',                        
+    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        
+    aws_secret_access_key: ENV["AWS_SECRET_KEY"],
+    aws_region: ENV["AWS_REGION"]      
   }
-
-  # Optional: Signing of download urls, e.g. for serving private content through
-  # CloudFront. Be sure you have the `cloudfront-signer` gem installed and
-  # configured:
-  # config.aws_signer = -> (unsigned_url, options) do
-  #   Aws::CF::Signer.sign_url(unsigned_url, options)
-  # end
+  config.fog_directory  = ENV["AWS_BUCKET"]            
 end
